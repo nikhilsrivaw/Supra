e0890f0ace9384230969eb3435240379161d7af5
nikhilsrivaw <nikhilksrivastav190@gmail.com>
2024-12-15 22:53:41 +0530

    content changed

--- src/components/particles/Data.tsx
+++ src/components/particles/Data.tsx
@@ -10,51 +10,48 @@ export const NavLinks = [
 
 export const HeroTexts = [
     {
-        Heading: "BAJA",
-        Paragraph: "Where Engineering Meets Adventure  BAJA SAE fosters collaboration, creativity, and problem-solving, preparing aspiring engineers for dynamic careers in the automotive and off-road industries. It’s not just about building vehicles—it’s about shaping future innovators. ",
+        Heading: "Supra",
+        Paragraph: "SUPRA SAE replicates the high-stakes environment of automotive design and production. Students design and construct Formula-style race cars, focusing on optimizing safety, efficiency, and performance. ",
         Button: "."
     },
     {
-        Heading: "New Heights of speeding!",
-        Paragraph: "Push your limits and achieve greatness in SAE Baja! Join a community of passionate engineering students and embark on a journey of design, innovation, and hands-on learning as you create off-road vehicles for tough terrains.",
+        Heading: "Building Future Automotive Innovators",
+        Paragraph: "This competition enhances their technical knowledge, teamwork, and project management abilities, equipping them for the demands of the automotive sector.",
         Button: "."
     },
     {
         Heading: "Off-Road Vehicle Challenges",
-        Paragraph: "At SAE Baja, engineering is more than just a competition; it's an exciting adventure! Join us in this thrilling experience, where creativity, teamwork, and skill combine to deliver outstanding results!",
+        Paragraph: "At SAE Supra, engineering is more than just a competition; it's an exciting adventure! Join us in this thrilling experience, where creativity, teamwork, and skill combine to deliver outstanding results!",
         Button: "."
     }
 ]
 
 
 export const AboutTexts = {
-    firstText: "About BAJA",
+    firstText: "About SUPRA ",
     secondText: "Welcome",
-    caption: "Design. Build. Conquer. Experience innovation and teamwork with rugged all-terrain vehicles at BAJA SAE!",
-    paragraph1: "BAJA SAE is an engineering design competition that challenges students to design, build, and race rugged all-terrain vehicles. The event emphasizes teamwork, innovation, and practical problem-solving, requiring participants to create vehicles capable of withstanding harsh terrains and dynamic conditions. It equips students with hands-on experience, preparing them for careers in the automotive and off-road vehicle industries.",
+    caption: "Design. Build. Conquer. Experience innovation and teamwork with rugged all-terrain vehicles at BAJA Supra!",
+    paragraph1: " SUPRA SAE is a prestigious engineering competition that simulates a real-world automotive design and manufacturing environment. Participants design, fabricate, and test Formula-style race cars while optimizing performance, efficiency, and safety. This competition sharpens technical expertise, project management, and teamwork, fostering skills essential for the competitive automotive industry.",
 }
 
 export const OfferTexts = {
     firstText: "What we do",
-    secondText: "BAJA",
+    secondText: "SUPRA",
     list: [
         {
-            listCaption: "Vehicle Design Programs",
-            text: "Gain hands-on experience in designing all-terrain vehicles with innovative features. Learn from industry experts and push the boundaries of automotive engineering."
+            listCaption: "Formula Car Design Programs",
+            text: "Dive into the art of designing high-performance formula-style cars. Learn cutting-edge techniques and gain practical experience in vehicle aerodynamics and innovation."
         },
         {
-            listCaption: "Teamwork & Collaboration Workshops",
-            text: "Enhance your teamwork skills through collaborative workshops, where creativity and problem-solving come together to tackle real-world challenges in vehicle design."
+            listCaption: "Teamwork & Engineering Collaboration",
+            text: "Strengthen your collaboration and problem-solving skills through workshops focused on real-world automotive challenges and efficient team dynamics."
         },
         {
-            listCaption: "Dynamic Testing Challenges",
-            text: "Participate in thrilling testing sessions that simulate real-world terrains. Test your vehicle's endurance, durability, and performance in challenging conditions."
-        },
-        // {
-        //     listCaption: "Innovation in Engineering",
-        //     text: "Unlock your potential by creating cutting-edge solutions in automotive engineering. Transform your ideas into impactful designs with hands-on learning."
-        // }
+            listCaption: "Dynamic Racing Challenges",
+            text: "Test your formula car's speed, handling, and endurance in thrilling on-track events. Experience the excitement of pushing your design to its limits under competitive conditions."
+        }
     ]
+    
     ,
 }
 
